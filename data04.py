4c8c552f1c2fc015e98f9453035456ca1eeb07bf
johnny-walker <albert_hsu@cyberlink.com>
2021-06-30 14:27:22 +0800

    add boars selection

--- data04.py
+++ data04.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import webbrowser
 
 import tkinter as tk
+import tkinter.ttk as ttk
 from Root import ProgramBase
 
 class PTTCrawler():
@@ -37,7 +38,9 @@ class PTTCrawler():
         return self.page
 
 class PTTViewer(ProgramBase):
-    def __init__(self, root, width=640, height=480):
+    postbg = '#ddeedd'
+
+    def __init__(self, root, width=800, height=600):
         super().__init__(root, width, height)
         self.root.title("PTT Posts")
         self.crawer = PTTCrawler()
@@ -51,44 +54,62 @@ class PTTViewer(ProgramBase):
         self.showPosts(self.posts)
     
     def loadLayout(self):
-        def defineLayout(widget, cols=1, rows=1):
-            #https://stackoverflow.com/questions/45847313/what-does-weight-do-in-tkinter
-            for c in range(cols):    
-                widget.columnconfigure(c, weight=1)
-            for r in range(rows):
-                widget.rowconfigure(r, weight=1)
-
         align_mode = 'nswe'
         padding= 2
         msgHeight = 40
+        optHeight = 40
+        self.dataWidth = self.root.width
+        self.dataHeight = self.root.height - optHeight - msgHeight
 
-        self.imgWidth = self.root.width
-        self.imgHeight = self.root.height - msgHeight
+        self.divOptions = tk.Frame(self.root,  width=self.dataWidth , height=optHeight)
+        self.divOptions.grid(row=0, columnspan=4, padx=padding, pady=padding, sticky=align_mode)
+        
+        self.divData = tk.Frame(self.root,  width=self.dataWidth , height=self.dataHeight , bg=self.postbg)
+        self.divData.grid(row=1, padx=padding, pady=padding, sticky=align_mode)
+        
+        self.divMsg = tk.Frame(self.root,  width=self.dataWidth , height=msgHeight , bg='black')
+        self.divMsg.grid(row=2, columnspan=4, padx=padding, pady=padding, sticky=align_mode)
 
-        self.divData = tk.Frame(self.root,  width=self.imgWidth , height=self.imgHeight , bg='white')
-        self.divData.grid(row=0, padx=padding, pady=padding, sticky=align_mode)
-        divMsg = tk.Frame(self.root,  width=self.imgWidth , height=msgHeight , bg='black')
-        divMsg.grid(row=1, columnspan=4, padx=padding, pady=padding, sticky=align_mode)
+        #define widgets' relative ratio )
+        self.root.rowconfigure(1, weight=1)
+        self.root.columnconfigure(0, weight=1)
 
-        defineLayout(self.root)
         self.divData.columnconfigure(0, weight=1)
-        self.divData.columnconfigure(1, weight=20)
-        self.divData.columnconfigure(2, weight=2)
-        self.divData.columnconfigure(3, weight=4)
+        self.divData.columnconfigure(1, weight=10)
+        self.divData.columnconfigure(2, weight=1)
+        self.divData.columnconfigure(3, weight=2)
 
         # label as message
-        self.lblMsg = tk.Label(divMsg, text='show message here', bg='black', fg='white')
+        self.lblMsg = tk.Label(self.divMsg, text='show message here', bg='black', fg='white')
         self.lblMsg.grid(row=0, column=0, sticky='w')
 
+    def showOptions(self, boards):
+        padding = 1
+        label = tk.Label( self.divOptions,  text=" PTT Board Selection: ")
+        label.grid(row=0, column=0, sticky='nswe')
+
+        combobox = ttk.Combobox(self.divOptions, values=boards, width=20, height=1)
+        combobox.current(0)   # select first one
+        combobox.grid(row=0, column=1, columnspan=3, padx=padding, pady=padding) 
+
+        combobox.bind('<<ComboboxSelected>>', self.optSelected)
+
+    def optSelected(self, event):
+        print(event.widget.get())
+        self.showPage(event.widget.get())
+
     def showPosts(self, posts):
+        padding =2
+        for child in self.divData.winfo_children():
+            child.destroy()
+
         for row in range(len(posts)):
             color = 'black'
-            print(posts[row]['push'], posts[row]['title'], posts[row]['date'], posts[row]['author'])
-            tk.Label(self.divData, fg=color, text=posts[row]['push']).grid(row=row, column=0, padx=5, pady=5, sticky='e')
-            title = tk.Label(self.divData, fg=color, text=posts[row]['title'])
-            title.grid(row=row, column=1, padx=5, pady=5, sticky='w')
-            tk.Label(self.divData, fg=color, text=posts[row]['date']).grid(row=row, column=2, padx=5, pady=5, sticky='w')
-            tk.Label(self.divData, fg=color, text=posts[row]['author']).grid(row=row, column=3, padx=5, pady=5, sticky='w')
+            tk.Label(self.divData, fg=color, bg=self.postbg, text=posts[row]['push']).grid(row=row, column=0, padx=padding, pady=padding, sticky='e')
+            title = tk.Label(self.divData, fg=color, bg=self.postbg, text=posts[row]['title'])
+            title.grid(row=row, column=1, padx=padding, pady=padding, sticky='w')
+            tk.Label(self.divData, fg=color, bg=self.postbg, text=posts[row]['date']).grid(row=row, column=2, padx=padding, pady=padding, sticky='w')
+            tk.Label(self.divData, fg=color, bg=self.postbg, text=posts[row]['author']).grid(row=row, column=3, padx=padding, pady=padding, sticky='w')
         
             # bind mouse events
             title.bind("<Enter>", self.onEnter)
@@ -113,7 +134,9 @@ class PTTViewer(ProgramBase):
 
 if __name__ == '__main__':
     program = PTTViewer(tk.Tk())
-    program.showPage('movie')
+    boards = ['NBA', 'MLB', 'Baseball', 'Music', 'Movie', 'Stock', 'Joke', 'KoreaStar','Boy-Girl', 'NSwitch','Car']
+    program.showOptions(boards)
+    program.showPage('NBA')
     program.run()
 
 
